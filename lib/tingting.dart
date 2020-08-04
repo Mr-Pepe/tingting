4136285647aee3e4fcf228502a44f5737154df40
Felipe Peter <mr-peipei@web.de>
2020-08-04 23:32:46 +0800

    Extract player controls to own file

--- lib/tingting.dart
+++ lib/tingting.dart
@@ -1,10 +1,7 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:provider/provider.dart';
+import 'package:tingting/playerControls.dart';
 import 'package:tingting/tingtingViewModel.dart';
-import 'package:tingting/utils.dart';
-import 'package:tingting/values/strings.dart';
 
 class TingTing extends StatefulWidget {
   TingTing({Key key}) : super(key: key);
@@ -13,175 +10,88 @@ class TingTing extends StatefulWidget {
   _TingTingState createState() => _TingTingState();
 }
 
-class _TingTingState extends State<TingTing> {
+class _TingTingState extends State<TingTing>
+    with SingleTickerProviderStateMixin {
+  final List<Tab> tabs = <Tab>[
+    Tab(text: 'Yours'),
+    Tab(text: 'Diff'),
+    Tab(text: 'Original'),
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: tabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final model = Provider.of<TingTingViewModel>(context);
 
     return Container(
       child: Center(
-        child: StreamBuilder<Duration>(
-          stream: model.player.durationStream,
-          builder: (context, snapshot) {
-            final duration = snapshot.data ?? Duration.zero;
+          child: Column(
+        children: <Widget>[
+          TabBar(
+            controller: _tabController,
+            tabs: tabs,
+            unselectedLabelColor: Colors.black,
+            labelColor: Colors.black,
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: <Widget>[
+                InputTextField(),
+                Text("2"),
+                Text("3"),
+              ],
+            ),
+          ),
+          PlayerControls(),
+        ],
+      )),
+    );
+  }
+}
 
-            return StreamBuilder<Duration>(
-              stream: model.player.getPositionStream(),
-              builder: (context, snapshot) {
-                Duration position = snapshot.data ?? Duration.zero;
-                if (position > duration) {
-                  position = duration;
-                }
-                return Column(
-                  children: <Widget>[
-                    ProgressBar(
-                      duration: duration,
-                      position: position,
-                      onChangeEnd: (newPosition) {
-                        model.player.seek(newPosition);
-                      },
-                    ),
-                    StreamBuilder<FullAudioPlaybackState>(
-                      stream: model.player.fullPlaybackStateStream,
-                      builder: (context, snapshot) {
-                        final fullState = snapshot.data;
-                        final state = fullState?.state;
-                        final buffering = fullState?.buffering;
+class InputTextField extends StatefulWidget {
+  @override
+  _InputTextFieldState createState() => _InputTextFieldState();
+}
 
-                        return SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            children: <Widget>[
-                              RaisedButton(
-                                child: Text(Strings.chooseAudioFile),
-                                onPressed: () async {
-                                  final audioFile = await FilePicker.getFile();
-                                  if (audioFile != null) {
-                                    model.setAudioFile(audioFile);
-                                  }
-                                },
-                              ),
-                              RaisedButton(
-                                child: (state != AudioPlaybackState.playing)
-                                    ? Text("Play")
-                                    : Text("Pause"),
-                                onPressed: () {
-                                  (state != AudioPlaybackState.playing)
-                                      ? model.player.play()
-                                      : model.player.pause();
-                                },
-                              ),
-                              RaisedButton(
-                                child: Text("Stop"),
-                                onPressed: () {
-                                  model.player.stop();
-                                },
-                              ),
-                              RaisedButton(
-                                child: Text("-5s"),
-                                onPressed: () {
-                                  model.player.seek(clampDuration(
-                                      position - Duration(seconds: 5),
-                                      Duration.zero,
-                                      duration));
-                                },
-                              ),
-                              RaisedButton(
-                                child: Text("+5s"),
-                                onPressed: () {
-                                  model.player.play();
-                                  model.player.pause();
-                                  model.player.seek(clampDuration(
-                                      position + Duration(seconds: 5),
-                                      Duration.zero,
-                                      duration));
-                                },
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        ),
+class _InputTextFieldState extends State<InputTextField> {
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<TingTingViewModel>(context);
+
+    return Container(
+      child: TextField(
+        controller: _textController,
+        onChanged: (value) {},
       ),
     );
-  }
-}
-
-class ProgressBar extends StatefulWidget {
-  final Duration duration;
-  final Duration position;
-  final ValueChanged<Duration> onChanged;
-  final ValueChanged<Duration> onChangeEnd;
-
-  ProgressBar({
-    @required this.duration,
-    @required this.position,
-    this.onChanged,
-    this.onChangeEnd,
-  });
-
-  @override
-  _ProgressBarState createState() => _ProgressBarState();
-}
-
-class _ProgressBarState extends State<ProgressBar> {
-  Duration _dragValue;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Slider(
-          min: 0.0,
-          max: widget.duration.inMilliseconds.toDouble(),
-          value: _dragValue?.inMilliseconds?.toDouble() ??
-              widget.position.inMilliseconds.toDouble(),
-          onChanged: (value) {
-            setState(() {
-              _dragValue = Duration(milliseconds: value.round());
-            });
-            if (widget.onChanged != null) {
-              widget.onChanged(Duration(milliseconds: value.round()));
-            }
-          },
-          onChangeEnd: (value) {
-            _dragValue = null;
-            if (widget.onChangeEnd != null) {
-              widget.onChangeEnd(Duration(milliseconds: value.round()));
-            }
-          },
-        ),
-        ProgressText(
-          duration: widget.duration,
-          position: _dragValue ?? widget.position,
-        )
-      ],
-    );
-  }
-}
-
-class ProgressText extends StatelessWidget {
-  final Duration duration;
-  final Duration position;
-  const ProgressText({@required this.duration, @required this.position});
-
-  @override
-  Widget build(BuildContext context) {
-    final text = duration == Duration.zero
-        ? "--/--"
-        : position.inMinutes.toString() +
-            ":" +
-            (position.inSeconds % 60).toString() +
-            " / " +
-            duration.inMinutes.toString() +
-            ":" +
-            (duration.inSeconds % 60).toString();
-    return Text(text);
   }
 }
