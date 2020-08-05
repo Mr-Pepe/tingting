0b6b74fba04bd441f303c35b117b54ccd7680050
Felipe Peter <mr-peipei@web.de>
2020-08-05 22:41:42 +0800

    Implement backtracing

--- lib/utils/alignment.dart
+++ lib/utils/alignment.dart
@@ -1,10 +1,12 @@
 import 'dart:math';
 
+import 'package:characters/characters.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
 
 class Aligner {
-  final String original;
-  final String query;
+  final Characters original;
+  final Characters query;
   final String placeholder;
   final int matchScore;
   final int mismatchScore;
@@ -12,6 +14,11 @@ class Aligner {
   final List<GlobalAlignment> alignments = [];
   List<List<int>> _scoreMatrix;
   List<List<int>> get scoreMatrix => _scoreMatrix;
+
+  List<List<List<List<int>>>> _backtraceMatrix;
+
+  int _maxScore;
+  int get maxScore => _maxScore;
 
   Aligner({
     @required this.original,
@@ -27,8 +34,76 @@ class Aligner {
   // Align original and query using Needleman-Wunsch algorithm
   void align() {
     initializeScoreMatrix();
+    initializeBacktraceMatrix();
 
     calculateScores();
+
+    backtrace(
+      [query.length, original.length],
+      _backtraceMatrix[query.length][original.length],
+      GlobalAlignment(original: ''.characters, query: ''.characters),
+    );
+  }
+
+  backtrace(
+    List<int> index,
+    List<List<int>> parents,
+    GlobalAlignment alignmentSoFar,
+  ) {
+    // Recursively backtrace through the score matrix
+    if (parents.isEmpty) {
+      alignments.add(GlobalAlignment(
+          original: alignmentSoFar.original.toList().reversed.join().characters,
+          query: alignmentSoFar.query.toList().reversed.join().characters));
+    } else {
+      for (var parent in parents) {
+        if (parent[0] == index[0] - 1 && parent[1] == index[1] - 1) {
+          alignmentSoFar = GlobalAlignment(
+              original: alignmentSoFar.original +
+                  original.elementAt(index[1] - 1).characters,
+              query: alignmentSoFar.query +
+                  query.elementAt(index[0] - 1).characters);
+        } else if (parent[0] == index[0] - 1) {
+          alignmentSoFar = GlobalAlignment(
+              original: alignmentSoFar.original + placeholder.characters,
+              query: alignmentSoFar.query +
+                  query.elementAt(index[0] - 1).characters);
+        } else if (parent[1] == index[1] - 1) {
+          alignmentSoFar = GlobalAlignment(
+              original: alignmentSoFar.original +
+                  original.elementAt(index[1] - 1).characters,
+              query: alignmentSoFar.query + placeholder.characters);
+        }
+
+        backtrace(
+          parent,
+          _backtraceMatrix[parent[0]][parent[1]],
+          alignmentSoFar,
+        );
+      }
+    }
+  }
+
+  initializeBacktraceMatrix() {
+    _backtraceMatrix = List.generate(
+      query.length + 1,
+      (iRow) => List.generate(
+        original.length + 1,
+        (iColumn) {
+          if (iRow == 0 && iColumn != 0) {
+            return [
+              [iColumn - 1, 0]
+            ];
+          } else if (iColumn == 0 && iRow != 0) {
+            return [
+              [iRow - 1, 0]
+            ];
+          } else {
+            return [];
+          }
+        },
+      ),
+    );
   }
 
   initializeScoreMatrix() {
@@ -53,28 +128,46 @@ class Aligner {
   calculateScores() {
     for (var i = 1; i < _scoreMatrix.length; i++) {
       for (var j = 1; j < _scoreMatrix[0].length; j++) {
-        int match = _scoreMatrix[i - 1][j - 1];
-        if ((query[i - 1] == original[j - 1])) {
-          match += matchScore;
-        } else {
-          match += mismatchScore;
+        final possibleParents = [
+          [i - 1, j - 1],
+          [i - 1, j],
+          [i, j - 1],
+        ];
+
+        int match = (query.elementAt(i - 1) == original.elementAt(j - 1))
+            ? matchScore
+            : mismatchScore;
+
+        final possibleScores = [
+          _scoreMatrix[i - 1][j - 1] + match,
+          _scoreMatrix[i - 1][j] + gapScore,
+          _scoreMatrix[i][j - 1] + gapScore
+        ];
+
+        final maxScore =
+            max(max(possibleScores[0], possibleScores[1]), possibleScores[2]);
+
+        _scoreMatrix[i][j] = maxScore;
+
+        // Save where the maximum score came from to allow easier backtracing
+        for (var iScore = 0; iScore < possibleScores.length; iScore++) {
+          if (possibleScores[iScore] == maxScore) {
+            _backtraceMatrix[i][j].add(possibleParents[iScore]);
+          }
         }
-
-        int delete = _scoreMatrix[i - 1][j] + gapScore;
-        int insert = _scoreMatrix[i][j - 1] + gapScore;
-
-        int score = max(delete, insert);
-        score = max(score, match);
-
-        _scoreMatrix[i][j] = score;
       }
     }
+
+    _maxScore = _scoreMatrix[query.length][original.length];
   }
 }
 
-class GlobalAlignment {
-  final String original;
-  final String query;
+class GlobalAlignment extends Equatable {
+  final Characters original;
+  final Characters query;
 
   GlobalAlignment({@required this.original, @required this.query});
+
+  @override
+  List<Object> get props => [original, query];
 }
--- test/alignment_test.dart
+++ test/alignment_test.dart
@@ -1,21 +1,21 @@
+import 'package:characters/characters.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tingting/utils/alignment.dart';
 
 void main() {
   final original = "CCATGAAU";
   final query = "GATTACA";
-
   final aligner = Aligner(
-    original: original,
-    query: query,
+    original: original.characters,
+    query: query.characters,
     matchScore: 1,
     mismatchScore: -1,
     gapScore: -1,
   );
 
   test('Alignment object saves the strings', () {
-    expect(aligner.original, original);
-    expect(aligner.query, query);
+    expect(aligner.original.toString(), original);
+    expect(aligner.query.toString(), query);
   });
 
   test('Score matrix has the correct size', () {
@@ -43,5 +43,24 @@ void main() {
         expect(aligner.scoreMatrix[iLine][iValue], scoreMatrix[iLine][iValue]);
       }
     }
+  });
+
+  test('Best score is correct', () {
+    expect(aligner.maxScore, -1);
+  });
+
+  test('Number of optimal alignments is correct', () {
+    expect(aligner.alignments.length, 2);
+  });
+
+  test('Alignments are correct', () {
+    expect(
+        aligner.alignments.contains(GlobalAlignment(
+            original: 'CCATGA-AU'.characters, query: '-GATTACA-'.characters)),
+        true);
+    expect(
+        aligner.alignments.contains(GlobalAlignment(
+            original: 'CCCATGA-AU'.characters, query: 'G-GATTACA-'.characters)),
+        true);
   });
 }
