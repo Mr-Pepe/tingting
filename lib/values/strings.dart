2a8df76a69efdbf5af84409cd8db474bb201cf85
Felipe Peter <mr-peipei@web.de>
2020-08-11 20:07:01 +0800

    Refactor diff text field

--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -1,3 +1,6 @@
 class Strings {
   static final chooseAudioFile = "Choose audio file";
+  static final noComparisonAvailable =
+      "No comparison available.\nIs there an original and a self-written text?";
+  static final oops = "Oops, something went wrong.";
 }
