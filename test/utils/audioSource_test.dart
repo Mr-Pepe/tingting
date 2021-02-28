b477661b1aa5ef096ec5a2fb89b39fdf4a551e86
Felipe Peter <mr-peipei@web.de>
2021-02-28 22:44:49 +0100

    Add more audio source tests

--- test/utils/audioSource_test.dart
+++ test/utils/audioSource_test.dart
@@ -1,10 +1,11 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tingting/utils/audioSource.dart';
 
 void main() {
-  group('Test buffer audio source', () {
+  group('Test buffer audio source on synthetic data', () {
     final buffer = Uint8List.fromList(List.generate(1000, (index) => index));
     final audioSource = BufferAudioSource(buffer);
 
@@ -19,6 +20,26 @@ void main() {
       final response = await audioSource.request();
       expect(response.sourceLength, equals(1000));
       expect(response.contentLength, equals(1000));
+      expect(response.offset, equals(0));
+    });
+  });
+
+  group('Test buffer audio source on real data', () {
+    final file = File('test_resources/test.mp3');
+    final buffer = file.readAsBytesSync();
+    final audioSource = BufferAudioSource(buffer);
+
+    test('Test stream request with start and end provided', () async {
+      final response = await audioSource.request(10, 20);
+      expect(response.sourceLength, equals(buffer.length));
+      expect(response.contentLength, equals(10));
+      expect(response.offset, equals(10));
+    });
+
+    test('Test stream request without start and end provided', () async {
+      final response = await audioSource.request();
+      expect(response.sourceLength, equals(buffer.length));
+      expect(response.contentLength, equals(buffer.length));
       expect(response.offset, equals(0));
     });
   });
